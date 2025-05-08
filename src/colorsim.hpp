b623747f9cde6920bcaaf4766d46d76029e6e2e1
Mark Busenitz <mbusenitz@gmail.com>
2025-05-07 22:09:19 -0700

    Wgui (#1)
    * local PI2
    
    * updates from wgui
    
    * format
    
    ---------
    
    Co-authored-by: mucbuc <mucbuc@mucbucs-Mac-mini-2.local>

--- src/colorsim.hpp
+++ src/colorsim.hpp
@@ -1,3 +1,14 @@
+/* example usage:
+
+cpp:
+m_colors(dawn, 27, 5)
+...
+m_colors.get_wgsl_code("ColorPalette", "getColor", entry_colors, 0) },
+
+wgsl:
+var color = getColor(color_factor + PHI_HALF);
+*/
+
 #pragma once
 
 #include <dawn_wrapper/src/dawn_wrapper.h>
@@ -10,10 +21,9 @@ struct ColorSim {
     ColorSim(dawn_wrapper::dawn_plugin dawn, unsigned count, unsigned padding = 0);
     ~ColorSim();
 
-    void simulate(float_t ft);
-    dawn_wrapper::buffer_wrapper elements_buffer();
-    void compute(dawn_wrapper::encoder_wrapper encoder);
-    static std::string get_wgsl_code(std::string class_name, std::string getter_name);
+    void simulate(float_t ft, dawn_wrapper::encoder_wrapper encoder);
+    dawn_wrapper::buffer_wrapper buffer();
+    static std::string get_wgsl_code(std::string class_name, std::string getter_name, unsigned entry, unsigned group);
 
 private:
     struct Pimpl;
